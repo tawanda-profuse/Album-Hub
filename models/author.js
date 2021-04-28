08416cbe49cf3079fa4ad3b2056b80991f8f057c
Tawanda Msengezi <xrthemc@gmail.com>
2021-04-28 12:52:22 +0300

    Updated UI

--- models/author.js
+++ models/author.js
@@ -13,7 +13,7 @@ authorSchema.pre('remove', function(next) {
     if (err) {
       next(err)
     } else if (books.length > 0) {
-      next(new Error('This author has books still'))
+      next(new Error('This artist has albums still'))
     } else {
       next()
     }
